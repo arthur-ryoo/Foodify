b33afc06f69c72f77f4f37fa7c41f9f96e856b01
arthur-ryoo <jene0523@gmail.com>
2020-03-02 23:03:09 -0800

    add logic

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from './Navbar.module.css';
 import { Link } from 'react-router-dom';
 
+import userService from '../../utils/userService';
+
 const Navbar = props => {
+  const conditionalUI = userService.getUser() ? (
+    <Fragment>
+      <li>
+        <Link to="/restaurants">Restaurants</Link>
+      </li>
+      <li>
+        <Link to="" onClick={props.handleLogout}>
+          Logout
+        </Link>
+      </li>
+    </Fragment>
+  ) : (
+    <Fragment>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/signup">Signup</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <nav className={styles.navbar}>
       <Link to="/">
         <h1>Foodify</h1>
       </Link>
-      <ul>
-        <li>
-          <Link to="/restaurants">Restaurants</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-      </ul>
+      <ul>{conditionalUI}</ul>
     </nav>
   );
 };
